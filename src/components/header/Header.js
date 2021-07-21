195bc209a51dbe31e2675db43209799bede8913e
Abhishek Parashar <abhishekparashar@Abhisheks-MacBook-Pro.local>
2021-07-21 21:06:48 +0530

    header option

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,6 @@ import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import HomeIcon from "@material-ui/icons/Home";
 import Logo from "../../img/linkedin.png";
-import Abhi from '../../img/abhi.jpeg'
 import HeaderOption from "../headerOption/HeaderOption";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
@@ -26,10 +25,7 @@ const Header = () => {
         <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
         <HeaderOption Icon={ChatIcon} title="Messaging" />
         <HeaderOption Icon={NotificationsIcon} title="Notifications" />
-        <HeaderOption
-          avatar="../../img/abhi.jpeg"
-          title="Abhishek"
-        />
+        <HeaderOption avatar="../../img/abhi.jpeg" title="Abhishek" />
       </div>
     </div>
   );
--- src/components/headerOption/HeaderOption.js
+++ src/components/headerOption/HeaderOption.js
@@ -2,12 +2,11 @@ import React from "react";
 import "./headerOption.css";
 import { Avatar } from "@material-ui/core";
 
-
-const HeaderOption = ({ avatar,Icon, title }) => {
+const HeaderOption = ({ avatar, Icon, title }) => {
   return (
     <div className="headeroption">
-      {Icon && <Icon/>}
-      {avatar && <Avatar src={avatar} alt="Me"/>}
+      {Icon && <Icon />}
+      {avatar && <Avatar src={avatar} alt="Me" />}
       {title}
     </div>
   );
