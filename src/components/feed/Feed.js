cfe96c33293fa411f897c64cf8851d39dbf80036
Abhishek Parashar <abhishekparashar@Abhisheks-MacBook-Pro.local>
2021-07-22 23:20:08 +0530

    input options

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Feed.css'
 import CreateIcon from "@material-ui/icons/Create"
+import ImageIcon from '@material-ui/icons/Image';
+import  SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import  EventNoteIcon  from '@material-ui/icons/EventNote'; 
+import  CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import InputOptions from '../../components/inputOptions/InputOptions.js'
 
 const Feed = () => {
     return (
@@ -12,6 +17,12 @@ const Feed = () => {
                         <input type="text"/>
                         <button type="submit">Send</button>
                     </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOptions Icon={ImageIcon} title="Photo" color="#70B5F9"/>
+                    <InputOptions Icon={SubscriptionsIcon} title="Video" color="#E7A33E"/>
+                    <InputOptions Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
+                    <InputOptions Icon={CalendarViewDayIcon} title="Write articles" color="#7FC15E"/>
                 </div>
             </div>
         </div>
